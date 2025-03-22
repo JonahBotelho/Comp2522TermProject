2c321abd82e4b8289eaf3ae01d5415617c067dce
Jonah Botelho <jbotelho3@my.bcit.ca>
2025-03-22 16:06:04 -0700

    Added abstract and concrete button classes

---
+++ src/code/ca/bcit/termproject/numbergame/WarningAlert.java
@@ -0,0 +1,24 @@
+package ca.bcit.termproject.numbergame;
+
+import javafx.scene.control.Alert;
+
+/**
+ * A custom alert for displaying warning messages to the user.
+ * Extends PopupAlert and uses Alert.AlertType.WARNING for warning messages.
+ *
+ * @author Jonah Botelho
+ * @version 1.0
+ */
+public class WarningAlert extends PopupAlert
+{
+
+    /**
+     * Constructs a WarningAlert with the specified title.
+     *
+     * @param title the title of the warning alert dialog
+     */
+    public WarningAlert(final String title)
+    {
+        super(AlertType.WARNING, title);
+    }
+}
