69684ff63d25705a156ad4d38d9253851569e8ad
Jonah Botelho <jbotelho3@my.bcit.ca>
2025-03-26 22:49:41 -0700

    Made playerSpeed a constant

--- src/code/ca/bcit/termproject/customgame/Player.java
+++ src/code/ca/bcit/termproject/customgame/Player.java
@@ -10,13 +10,14 @@ import javafx.scene.shape.Rectangle;
 public class Player extends Rectangle
 {
     private static final int NOTHING = 0;  // Boundary check constant
+    private static final double speed = 3;  // Movement speed of the player
 
     private boolean movingLeft = false;
     private boolean movingRight = false;
     private boolean movingUp = false;
     private boolean movingDown = false;
 
-    private final double speed = 3;  // Movement speed of the player
+
 
     /**
      * Constructs a Player object at the specified position with the given size.
